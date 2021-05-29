5bdaa51596c05a38965064b9525105f480549dae
Rook <raymondgonzalez22@gmail.com>
2021-05-28 21:58:19 -0400

    Update run.sh

--- generators/run.sh
+++ generators/run.sh
@@ -1,1 +1,39 @@
 
+#!/bin/bash
+
+#FILE NAME
+#PROGRAMTYPE
+
+date +”%m/%d/%Y”
+
+cd ..
+
+read dirname
+mkdir dirname
+cd dirname
+
+if [ -d "$dirname" ] 
+then
+    echo "dirname already exists";
+    # if not, exit with an exit code != 0
+    exit 2;
+fi
+
+
+mkdir src
+mkdir generators
+mkdir utilities
+cd src
+mkdir html
+mkdir css
+mkdir js
+touch home.html
+cd js
+touch home.js
+cd ..
+cd css 
+touch home.css 
+cd ..
+cd ..
+touch readme.txt
+ 
